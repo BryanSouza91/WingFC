fef5f0c69e3563eb2ecaaf76f56d44b0fb0ab5a8
Bryan Souza <brybryflys@gmail.com>
2025-09-19 21:25:06 -0700

    fixed some major bugs related to packet size with CRSF protocol;

--- firmware/src/helpers.go
+++ firmware/src/helpers.go
@@ -75,7 +75,10 @@ func constrain(value, min, max float64) float64 {
 }
 
 // Helper function to map a value from one range to another.
-func mapRange[T constraints.Float](value, fromMin, fromMax, toMin, toMax T) T {
+func mapRangeFloat[T constraints.Float](value, fromMin, fromMax, toMin, toMax T) T {
+	return (value-fromMin)/(fromMax-fromMin)*(toMax-toMin) + toMin
+}
+func mapRangeInt[T constraints.Integer](value, fromMin, fromMax, toMin, toMax T) T {
 	return (value-fromMin)/(fromMax-fromMin)*(toMax-toMin) + toMin
 }
 
@@ -91,55 +94,4 @@ func setServoPWM(leftPulse, rightPulse uint32) {
 func setESC(pulseWidth uint32) {
 	// The ESC's channel is typically CH3
 	pwm1.Set(pwmCh3, pulseWidth)
-}
-
-// Helper functions for reading IMU data, processing it, and calibration.
-func readLSMData() {
-	// Read raw sensor data from the IMU
-	rawAccelX, rawAccelY, rawAccelZ, err := lsm.ReadAcceleration()
-	if err != nil {
-		println("Error reading acceleration:", err)
-	}
-	rawGyroX, rawGyroY, rawGyroZ, err := lsm.ReadRotation()
-	if err != nil {
-		println("Error reading rotation:", err)
-	}
-
-	// Convert raw sensor readings to standard units (rad/s and m/s^2)
-	imuData.AccelX = float64(rawAccelX) * microGToMS2
-	imuData.AccelY = float64(rawAccelY) * microGToMS2
-	imuData.AccelZ = float64(rawAccelZ) * microGToMS2
-	imuData.GyroX = float64(rawGyroX) * microDPSToRadS
-	imuData.GyroY = float64(rawGyroY) * microDPSToRadS
-	imuData.GyroZ = float64(rawGyroZ) * microDPSToRadS
-}
-
-// Process IMU data by applying calibration offsets and computing angles.
-func processLSMData() {
-	imuData.AccelX -= accelBiasX
-	imuData.AccelY -= accelBiasY
-	imuData.AccelZ -= accelBiasZ
-	imuData.GyroX -= gyroBiasX
-	imuData.GyroY -= gyroBiasY
-	imuData.GyroZ -= gyroBiasZ
-	imuData.Roll = imuData.rollAccel()
-	imuData.Pitch = imuData.pitchAccel()
-}
-
-// Calibrate the IMU by averaging a number of samples to determine biases.
-func calibrate() {
-	const sampleSize = 4000
-
-	for i := 0; i < sampleSize; i++ {
-		readLSMData()
-	}
-
-	accelBiasX = accelXSum / sampleSize
-	accelBiasY = accelYSum / sampleSize
-	accelBiasZ = accelZSum / sampleSize
-	println("Accel calibration complete. Bias X:", accelBiasX, "Bias Y:", accelBiasY, "Bias Z:", accelBiasZ)
-	gyroBiasX = gyroXSum / sampleSize
-	gyroBiasY = gyroYSum / sampleSize
-	gyroBiasZ = gyroZSum / sampleSize
-	println("Gyro calibration complete. Bias X:", gyroBiasX, "Bias Y:", gyroBiasY, "Bias Z:", gyroBiasZ)
 }
